5117ea956b6a21d0053d3e1f1962c454cc9a43a1
Alexey Zyukov <aalex64@ukr.net>
2021-12-20 22:47:04 +0200

    initial update

--- src/App.js
+++ src/App.js
@@ -1,71 +1,66 @@
-import logo from './logo.svg';
-import PropTypes from 'prop-types';
 import './App.css';
 
-import { Fragment } from 'react';
-import { Component } from 'react';
+import { useState, Fragment } from 'react';
 import Section from './components/Section/Section';
 
 import styles from './components/feedback.module.css';
 import FeedbackOptions from './components/FeedbackOptions/FeedbackOptions';
 import Statistics from './components/Statistics/Statistics';
 
-class App extends Component {
-  static PropsTypes = {
-    good: PropTypes.number,
-    neutral: PropTypes.number,
-    bad: PropTypes.number,
-  };
+const initState = {
+  good: 0,
+  neutral: 0,
+  bad: 0,
+};
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      good: 0,
-      neutral: 0,
-      bad: 0,
-    };
-  }
+export default function App() {
+  const [state, setState] = useState(initState);
 
-  onBtnClick = param => {
-    this.setState(prevState => {
+  const onBtnClick = item => {
+    setState(prevState => {
+      console.log(state);
       return {
-        [param]: prevState[param] + 1,
+        [item]: prevState[item] + 1,
       };
     });
   };
 
-  countTotalFeedback = () => {
-    return Object.values(this.state).reduce((acc, value) => acc + value, 0);
+  const arrayOfStateEntry = () => {
+    console.log(Object.entries(state));
+    return Object.entries(state);
   };
-  countPositiveFeedbackPercentage = () => {
-    const { good } = this.state;
-    const total = this.countTotalFeedback();
+
+  const countTotalFeedback = () => {
+    console.log(state);
+    return Object.values(state).reduce((acc, value) => acc + value, 0);
+  };
+
+  const countPositiveFeedbackPercentage = () => {
+    const { good } = state;
+    console.log(good);
+    const total = countTotalFeedback();
     return total ? Math.round((good / total) * 100) : 0;
   };
 
-  render() {
-    return (
-      <Fragment>
-        <div className={styles.wrapper}>
-          <Section title="Please leave feedback">
-            <FeedbackOptions
-              arrayKeysFromState={Object.keys(this.state)}
-              onClickFn={this.onBtnClick}
-            />
-          </Section>
+  return (
+    <Fragment>
+      <div className={styles.wrapper}>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            arrayKeysFromState={Object.keys(state)}
+            onClickFn={onBtnClick}
+          />
+        </Section>
 
-          <Section title="Statistics">
-            <Statistics
-              totalFeedback={this.countTotalFeedback()}
-              positiveFeedback={this.countPositiveFeedbackPercentage()}
-              arrayFromStateEntry={Object.entries(this.state)}
-              message="There is no feedback"
-            />
-          </Section>
-        </div>
-      </Fragment>
-    );
-  }
+        <Section title="Statistics">
+          <Statistics
+            totalFeedback={countTotalFeedback()}
+            positiveFeedback={countPositiveFeedbackPercentage()}
+            arrayFromStateEntry={arrayOfStateEntry()}
+            message="There is no feedback"
+          />
+        </Section>
+      </div>
+    </Fragment>
+  );
 }
-
-export default App;
